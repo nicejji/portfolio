6e318a78347874e132f51e09c123adb9bfe4ca96
nicejji <nicejji@icloud.com>
2023-09-09 22:09:15 +0300

    move to bun js runtime

--- src/lib/components/Kitten.svelte
+++ src/lib/components/Kitten.svelte
@@ -1,35 +1,34 @@
 <script lang="ts">
-  import { scale } from "svelte/transition";
-  import BoxIcon from "~icons/solar/box-bold-duotone";
-  import LoadingIcon from "~icons/line-md/loading-loop";
-  export let opened = false;
-  let loaded = false;
-  export let src =
-    "https://freepngimg.com/save/169113-kitten-free-download-png-hq/1520x934";
+	import { scale } from 'svelte/transition';
+	import BoxIcon from '~icons/solar/box-bold-duotone';
+	import LoadingIcon from '~icons/line-md/loading-loop';
+	export let opened = false;
+	let loaded = false;
+	export let src = 'https://freepngimg.com/save/169113-kitten-free-download-png-hq/1520x934';
 </script>
 
 <button
-  class="grid"
-  on:click={() => (opened = true)}
-  style:animation="gliding-animation 7s infinite ease-in-out"
+	class="grid"
+	on:click={() => (opened = true)}
+	style:animation="gliding-animation 7s infinite ease-in-out"
 >
-  {#if opened}
-    <img
-      in:scale
-      on:load={() => (loaded = true)}
-      class="col-start-1 col-end-2 row-start-1 row-end-2 h-32 rounded-lg"
-      class:hidden={!loaded}
-      {src}
-      alt="Cat"
-    />
-    {#if !loaded}
-      <div class="col-start-1 col-end-2 row-start-1 row-end-2" transition:scale>
-        <LoadingIcon class="text-9xl bg-highlight-low rounded-lg" />
-      </div>
-    {/if}
-  {:else}
-    <div class="col-start-1 col-end-2 row-start-1 row-end-2" out:scale>
-      <BoxIcon class="text-9xl text-muted hover:scale-125 transition-all" />
-    </div>
-  {/if}
+	{#if opened}
+		<img
+			in:scale
+			on:load={() => (loaded = true)}
+			class="col-start-1 col-end-2 row-start-1 row-end-2 h-32 rounded-lg"
+			class:hidden={!loaded}
+			{src}
+			alt="Cat"
+		/>
+		{#if !loaded}
+			<div class="col-start-1 col-end-2 row-start-1 row-end-2" transition:scale>
+				<LoadingIcon class="text-9xl bg-highlight-low rounded-lg" />
+			</div>
+		{/if}
+	{:else}
+		<div class="col-start-1 col-end-2 row-start-1 row-end-2" out:scale>
+			<BoxIcon class="text-9xl text-muted hover:scale-125 transition-all" />
+		</div>
+	{/if}
 </button>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,9 @@
-import type { PageServerLoad } from "./$types";
-import { CAT_API_KEY } from "$env/static/private";
-import { TheCatAPI } from "@thatapicompany/thecatapi";
+import type { PageServerLoad } from './$types';
+import { CAT_API_KEY } from '$env/static/private';
+import { TheCatAPI } from '@thatapicompany/thecatapi';
 
 const catApi = new TheCatAPI(CAT_API_KEY);
 
 export const load: PageServerLoad = async () => ({
-  images: (await catApi.images.searchImages({ limit: 10 })).map(
-    (image) => image.url
-  ),
+	images: (await catApi.images.searchImages({ limit: 10 })).map((image) => image.url)
 });
